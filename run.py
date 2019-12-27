7764752bf5112d85169d5099f73f28186bd264c0
Yuqing Du <yuqing_du@hotmail.com>
2019-12-27 09:15:10 -0800

    Add test all function

--- run.py
+++ run.py
@@ -5,7 +5,42 @@ from datetime import datetime
 import os
 from emp_by_counting import EmpowermentCountingPolicy
 
-def run_gridworld_counting_policy(account_for_human, goal_oriented, results_folder, trial_num, test_case='center', grid_size=5):
+def rollout(env, results_folder, trial_num, horizon=10, num_traj=100, estimate_emp=True, account_for_human=False, goal_oriented=False):
+    policy = EmpowermentCountingPolicy(env, horizon=10, num_traj=100, estimate_emp=True, account_for_human=account_for_human, goal_oriented=goal_oriented)
+
+    s = env.reset()
+    num_steps = 1000
+
+    filename =  results_folder + "/trial_num_" + str(trial_num)
+    if account_for_human:
+       filename+="_account_for_human"
+    if goal_oriented:
+       filename+="_goal_oriented"
+    filename+=".txt"
+
+    file = open(filename, "w")
+    file.write("----------------- NEW EXPERIMENT ----------------")
+    file.close()
+    env.render(filename)
+    env.render()
+
+    for step in range(num_steps):
+        s, done = env.step_human(s)
+        if done:
+            file = open(filename, "a")
+            file.write("Reached Goal, took {} steps".format(step))
+            file.close()
+            break
+        action = policy.next_action(s)
+        env.set_state(s)
+        next_s, _, _, _ = env.step(action)
+        env.render(filename)
+        env.render()
+        s = next_s
+
+    return step
+
+def run_gridworld_counting_policy(account_for_human, goal_oriented, test_all, results_folder, trial_num, test_case='center', grid_size=5):
     #Initialize env and boxes location
     if "center" in test_case:
         center_coord = int(grid_size/2)
@@ -42,40 +77,21 @@ def run_gridworld_counting_policy(account_for_human, goal_oriented, results_fold
     # create env
     env = GridWorldEnv(seed=1, grid_size=grid_size, p=1.0, human_pos=human_pos, boxes_pos=boxes_pos, human_goal=human_goal)
 
-    policy = EmpowermentCountingPolicy(env, horizon=10, num_traj=1000, estimate_emp=True, account_for_human=account_for_human, goal_oriented=goal_oriented)
+    if test_all:
+        step_s1 = rollout(env, results_folder, trial_num, horizon=10, num_traj=100, estimate_emp=True, account_for_human=False, goal_oriented=False)
+        step_s2 = rollout(env, results_folder, trial_num, horizon=10, num_traj=100, estimate_emp=True, account_for_human=True, goal_oriented=False)
+        step_s3 = rollout(env, results_folder, trial_num, horizon=10, num_traj=100, estimate_emp=True, account_for_human=True, goal_oriented=True)
+        return [step_s1, step_s2, step_s3]
 
-    s = env.reset()
-    num_steps = 1000
-    filename =  results_folder + "/trial_num_" + str(trial_num) + ".txt"
-
-    file = open(filename, "w")
-    file.write("----------------- NEW EXPERIMENT ----------------")
-    file.close()
-    env.render(filename)
-    env.render()
-
-    for step in range(num_steps):
-        s, done = env.step_human(s)
-        if done:
-            file = open(filename, "a")
-            file.write("Reached Goal, took {} steps".format(step))
-            file.close()
-            break
-        action = policy.next_action(s)
-        env.set_state(s)
-        next_s, _, _, _ = env.step(action)
-        env.render(filename)
-        env.render()
-        s = next_s
-
-
-    return step
+    else:
+        return rollout(env, results_folder, trial_num, horizon=10, num_traj=100, estimate_emp=True, account_for_human=account_for_human, goal_oriented=goal_oriented)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Gridworld for Empowerment')
     parser.add_argument('--account_for_human', action='store_true', help='Compute human empowerment')
     parser.add_argument('--goal_oriented', action='store_true', help='Human is goal-oriented')
+    parser.add_argument('--test_all', action='store_true', help='Run through s1, s2, s3')
     parser.add_argument('--grid_size', type=int, default=5, help="Size of grid")
     parser.add_argument('--test_case', type=str, default='', help='Test Case -- \'center\' for human in center, \'corner\' for human in corner, default is random')
     parser.add_argument('--num_boxes', type=int, default=5, help='Number of boxes in scene')
@@ -90,6 +106,7 @@ if __name__ == '__main__':
     test_case = args.test_case
     num_boxes = args.num_boxes
     num_trials = args.num_trials
+    test_all = args.test_all
 
 
     if not os.path.exists('data'):
@@ -99,24 +116,36 @@ if __name__ == '__main__':
 
     date = now.strftime("%m-%d-%Y-%H-%M-%S")
     results_folder = 'data/'+date+"_"+test_case
-    if account_for_human:
-        results_folder+="_account_for_human"
-    if goal_oriented:
-        results_folder+="_goal_oriented"
+    if test_all:
+        results_folder += "_test_all"
+    else:
+        if account_for_human:
+            results_folder+="_account_for_human"
+        if goal_oriented:
+            results_folder+="_goal_oriented"
 
     if not os.path.exists(results_folder):
         os.makedirs(results_folder)
 
     steps = []
     for i in range(num_trials):
-        num_steps = run_gridworld_counting_policy(account_for_human, goal_oriented, results_folder, i, test_case, grid_size)
+        num_steps = run_gridworld_counting_policy(account_for_human, goal_oriented, test_all, results_folder, i, test_case, grid_size)
         steps.append(num_steps)
 
+    import ipdb; ipdb.set_trace()
     filename =  results_folder + "/summary.txt"
     file = open(filename, "w")
     file.write("----------------- SUMMARY ---------------- \n")
-    file.write('Mean steps to goal:' + str(np.mean(steps)) + " \n")
-    file.write('Std steps to goal:' + str(np.std(steps))+ " \n")
-    file.write('Max steps to goal:' + str(np.max(steps)) + " \n")
-    file.write('Min steps to goal:' + str(np.min(steps))+ " \n")
+
+    if test_all:
+        file.write('Mean steps to goal:' + str(np.mean(steps, axis=0)) + " \n")
+        file.write('Std steps to goal:' + str(np.std(steps,axis=0))+ " \n")
+        file.write('Max steps to goal:' + str(np.max(steps,axis=0)) + " \n")
+        file.write('Min steps to goal:' + str(np.min(steps,axis=0))+ " \n")
+
+    else:
+        file.write('Mean steps to goal:' + str(np.mean(steps)) + " \n")
+        file.write('Std steps to goal:' + str(np.std(steps))+ " \n")
+        file.write('Max steps to goal:' + str(np.max(steps)) + " \n")
+        file.write('Min steps to goal:' + str(np.min(steps))+ " \n")
     file.close()
